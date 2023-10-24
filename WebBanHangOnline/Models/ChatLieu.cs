c1bd09b0db1b0907b44c494488a4f1df157ffeb1
NguyenHoai1236954 <2051012028hoai@ou.edu.vn>
2023-10-24 23:39:03 +0700

    first commit

---
+++ WebBanHangOnline/Models/ChatLieu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebBanHangOnline.Models
+{
+    public partial class ChatLieu
+    {
+        public ChatLieu()
+        {
+            DanhMucSps = new HashSet<DanhMucSp>();
+        }
+
+        public string MaChatLieu { get; set; } = null!;
+        public string? ChatLieu1 { get; set; }
+
+        public virtual ICollection<DanhMucSp> DanhMucSps { get; set; }
+    }
+}
